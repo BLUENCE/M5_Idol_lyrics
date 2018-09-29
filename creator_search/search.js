462d3451a656912a4f8f8f2fee30b1338375274a
Yewon <jyw0754@gmail.com>
2018-09-30 02:31:38 +0900

    search

--- creator_search/search.js
+++ creator_search/search.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#left-keyword").keyup(function() {
     var k = $(this).val();
     $("#lyricist-table > tbody > tr").hide();
-    var temp = $("#lyricist-table > tbody > tr > td:nth-child(2n):contains('" + k + "')");
+    var temp = $("#lyricist-table > tbody > tr > td:nth-child(2n-1):contains('" + k + "')");
 
     $(temp).parent().show();
   })
@@ -12,7 +12,7 @@ $(document).ready(function() {
   $("#right-keyword").keyup(function() {
     var k = $(this).val();
     $("#lyricist-table > tbody > tr").hide();
-    var temp = $("#lyricist-table > tbody > tr > td:nth-child(2n+1):contains('" + k + "')");
+    var temp = $("#lyricist-table > tbody > tr > td:nth-child(2n):contains('" + k + "')");
 
     $(temp).parent().show();
   })
