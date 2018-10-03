0fc4c20e528df90843aff307ad9bb93966ac45ea
Yewon <jyw0754@gmail.com>
2018-10-04 03:02:01 +0900

    search update

--- creator_search/search.js
+++ creator_search/search.js
@@ -2,16 +2,6 @@ $(document).ready(function() {
   $("#left-keyword").keyup(function() {
     var k = $(this).val();
     $("#lyricist-table > tbody > tr").hide();
-    var temp = $("#lyricist-table > tbody > tr > td:nth-child(2n-1):contains('" + k + "')");
-
-    $(temp).parent().show();
-  })
-})
-
-$(document).ready(function() {
-  $("#right-keyword").keyup(function() {
-    var k = $(this).val();
-    $("#lyricist-table > tbody > tr").hide();
     var temp = $("#lyricist-table > tbody > tr > td:nth-child(2n):contains('" + k + "')");
 
     $(temp).parent().show();
